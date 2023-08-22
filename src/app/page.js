d537765f56a79d6ca078f29483e93dd6a71876db
guilhermesmb <guilherme.smbarbosa@gmail.com>
2023-08-21 22:19:06 -0300

    Site Ok

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,41 @@
 import Image from 'next/image'
+import Titulo from "@/components/Titulo";
 
 export default function Home() {
-  return (
-    <h1>Fiap Games</h1>
+  return ( // JSX
+  <>
+    <nav className='flex p-4 bg-sky-800'>
+        <ul className='flex gap-20'>
+          <li>
+            <a href='#'>
+              <h1>FiapGames</h1>  
+            </a>
+          </li>
+          <li>
+            <a href='#'>
+              Favoritos
+            </a>
+          </li>
+          <li>
+            <a href='#'>
+              PC
+            </a>
+          </li>
+          <li>
+            <a href='#'>
+              Xbox
+            </a>
+          </li>
+          <li>
+            <a href='#'>
+              PlayStation
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <Titulo>Favoritos</Titulo>
+      <Titulo>Ofertas</Titulo>
+  </>
   )
 }
---
+++ src/components/Titulo.js
@@ -0,0 +1,8 @@
+
+export default function Titulo({children}){
+    return(
+        <h2 className="text-2xl border-l-8 border-sky-800 m-4 pl-2 leading-6">
+            {children}
+        </h2>
+    )
+}
