eec5e18a7baee57c77c96103e95c0791a586c8ea
guilhermesmb <guilherme.smbarbosa@gmail.com>
2023-08-22 22:27:31 -0300

    Div Jogos

--- src/app/page.js
+++ src/app/page.js
@@ -35,6 +35,16 @@ export default function Home() {
       </nav>
 
       <Titulo>Favoritos</Titulo>
+
+      <div id="card" className='flex flex-col w-40 justify-center items-center'>
+      <img className='rounded' src='https://place-hold.it/150x220/666' alt=''/>
+      <span className='font-bold text-center line-clamp-1' >Titulo do Filme Card</span>
+      <div>
+        <span>6.0</span>
+      </div>
+      <a href='#' className='bg-sky-800 py-2 w-full rounded text-center' >detalhes</a>
+      </div>
+
       <Titulo>Ofertas</Titulo>
   </>
   )
