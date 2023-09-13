44a405d5c552a602686458497fea6f7fae9cb23b
guilhermesmb <guilherme.smbarbosa@gmail.com>
2023-09-12 22:24:55 -0300

    api com post

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,20 @@
 "use client"
 
 import { StarIcon } from '@heroicons/react/24/outline'
-import { useState } from 'react'
+import useFavorito from '@/hooks/jogos'
+
 
 export default function Card({jogo}){
 
-    const [favorito, setFavorito] = useState(true)
+    const { favorito, desfavoritar, favoritar } = useFavorito()
 
     return(
         <div id="card" className='flex flex-col p-2 w-40 justify-center items-center'>
 
         { favorito ?
-        <StarIcon onClick= {() => setFavorito(false)} className="h-6 w-6 text-yellow-300 cursor-pointer" />
+        <StarIcon onClick= {() => desfavoritar(jogo)} className="h-6 w-6 text-yellow-300 cursor-pointer" />
         :
-        <StarIcon onClick= {() => setFavorito(true)} className="h-6 w-6 text-gray-300 cursor-pointer" />
+        <StarIcon onClick= {() => favoritar(jogo)} className="h-6 w-6 text-gray-300 cursor-pointer" />
         }
 
         <img className='rounded' src={jogo.thumbnail} alt=''/>
